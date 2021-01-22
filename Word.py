e132f21269888f7bfe594843056a865a3a5dd0dc
Kedar Abhyankar <krabhyankar@gmail.com>
2021-01-22 16:14:38 -0500

    Created Word class for definition of words as pulled into database from Dictionary API.

---
+++ Word.py
@@ -0,0 +1,13 @@
+class Word:
+
+    def __init__(self, defined_word, related_words, antonyms, synonyms, alternate_spellings, defintion):
+        self.defined_word = defined_word
+        self.related_words = related_words
+        self.antonyms = antonyms
+        self.synonyms = synonyms
+        self.alternate_spellings = alternate_spellings
+        self.definition = defintion
+
+
+
+
